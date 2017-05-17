325cd61dea973a7f63f40f8d4e6199de579e9a04
Tim Adams <tim-adams@gmx.net>
2017-05-17 13:26:39 +0200

    Grundgerüst

---
+++ Blatt03/Neuron.java
@@ -0,0 +1,17 @@
+
+public class Neuron {
+
+	//input gewichtsvektor
+	double inputWeight;
+	double output;
+	
+public Neuron(double inputWeight) {
+	this.inputWeight = inputWeight;
+	this.output = inputWeight* //fermi funktion * "input;
+}
+	
+	
+	
+	
+	
+}
