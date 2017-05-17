55963549f3e3e6d5e48548d8927d276db2939376
Tim Adams <tim-adams@gmx.net>
2017-05-18 00:40:42 +0200

    Ich kapituliere hiermit feierlich. Mein Gehirn ist fertig für heute sagt es mir. Morgen vielleicht

---
+++ Blatt03/hardcode/Netzwerk.java
@@ -0,0 +1,15 @@
+package hardcode;
+
+import java.util.ArrayList;
+
+public class Netzwerk {
+	
+	ArrayList<Schicht> schichten;
+	
+	public void addSchicht(Schicht s){
+		schichten.add(s);
+	}
+	
+	
+	
+}
---
+++ Blatt03/hardcode/Neuron.java
@@ -0,0 +1,14 @@
+package hardcode;
+
+public class Neuron {
+	
+	double inputWeight;
+	double output;
+	
+	
+	Neuron(double inputWeight){
+		this.inputWeight = inputWeight;
+	}
+	
+	
+}
