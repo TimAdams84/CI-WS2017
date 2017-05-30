280c188d58aaa89177ff6ed02a3a4fab366a38ed
adamst <adamst@KUCKUCK.informatik.uni-bonn.de>
2017-05-30 15:36:25 +0200

    com

---
+++ Blatt04/Main.java
@@ -0,0 +1,28 @@
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+		
+		
+		//[Klasse][x1/x2][y]
+		double[][] 
+		
+		
+		
+		
+		for (int i = 0; i<2;i++){
+			for (int j=1;j<101;j++){
+				
+			}
+			
+			
+			
+		}
+		
+		
+		
+		
+	}
+	
+}
