a91ce1f3d2c118f6c050ce735131c3ac0abe57d4
pelzerm <pelzer.moritz@gmail.com>
2017-05-31 13:46:54 +0200

    getLayer hinzugefügt

--- Blatt04/neuralNetwork/Network.java
+++ Blatt04/neuralNetwork/Network.java
@@ -4,16 +4,25 @@ import java.util.ArrayList;
 
 public class Network {
 
-	ArrayList<Layer> layer = new ArrayList<Layer>();
+	ArrayList<Layer> layers = new ArrayList<Layer>();
 	double learningRate;
 	
+	public Network(double lr){
+		this.learningRate=lr;
+	}
+	
 	public void addLayer(Layer l){
-		layer.add(l);
+		layers.add(l);
+	}
+	
+	public Layer getLayer(int i){
+		Layer l = layers.get(i);
+		return l;
 	}
 	
 	public void feedForward(){
-		for(int i = 1; i<layer.size(); i++){
-			for (Neuron neuron : layer.get(i).getNeuronen()) {
+		for(int i = 1; i<layers.size(); i++){
+			for (Neuron neuron : layers.get(i).getNeuronen()) {
 				neuron.calculateOuput();
 			}
 		}
@@ -21,20 +30,20 @@ public class Network {
 	
 	public double getOutputError(){
 		double sumError = 0;
-		for (Neuron neuron :layer.get(layer.size()-1).getNeuronen()){
-			sumError += ((OutputNeuron)neuron).error;
+		for (Neuron neuron :layers.get(layers.size()-1).getNeuronen()){
+			sumError += 1/2*Math.pow(((OutputNeuron)neuron).error,2);
 		}
 	return sumError;
 	}
 	
 	public void backpropagate(double expected){
-		for (Neuron neuron :layer.get(layer.size()-1).getNeuronen()){
-			sumError += ((OutputNeuron)neuron).error;
+		for (Neuron neuron :layers.get(layers.size()-1).getNeuronen()){
+			neuron.backpropagate(expected);
 		}
 	}
 	
 	public double getSingleOutput(){
-		return layer.get(layer.size()-1).getNeuronen().get(0).output;
+		return layers.get(layers.size()-1).getNeuronen().get(0).output;
 	}
 	
 	
