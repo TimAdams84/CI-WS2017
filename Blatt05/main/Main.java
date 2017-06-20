e33f67c4a9e8e9c63ec6aba090b6c4f72c1e903c
Tim Adams <tim-adams@gmx.net>
2017-06-20 17:08:51 +0200

    winner ausgelagert

--- Blatt05/main/Main.java
+++ Blatt05/main/Main.java
@@ -17,6 +17,22 @@ public class Main {
 		return Math.sqrt(Math.pow(x1-x2, 2)+Math.sqrt(Math.pow(y1-y2, 2)));
 	}
 	
+	public static int getWinner(double[][] data, double[][] units){
+		int winnerIndex = 0;
+		double minDistance =  Double.POSITIVE_INFINITY;
+		for (int i = 0; i < units[0].length; i++) {
+			double datax = data[0][i];
+			double datay = data[1][i];
+			double currentDistance = getDistance(datax, datay, units[0][i],units[1][i]);
+			if (currentDistance< minDistance) {
+				winnerIndex = i;
+				minDistance = currentDistance;
+			}	
+			System.out.println("Datenpunkt "+i+" hat Gewinner-Unit mit Index "+winnerIndex);
+		}
+		return winnerIndex;
+	}
+	
 	public static void main(String[] args){
 	
 		double[][] data = new double[2][1001];
@@ -44,54 +60,23 @@ public class Main {
 		
 		//ordne Soms nach x-Werten aufsteigend, momentan Selection Sort
 		//sollte evtl als Merge Sort implementiert werden je nach Laufzeit
-		double tempx;
-		double tempy;
-		for(int i=0;i<99;i++){
-			for(int j=i+1; j<100;j++){
-				if (units[0][i]>units[0][j]){
-					tempx = units[0][i];
-					tempy = units[1][i];
-					units[0][i] = units[0][j];
-					units[1][i] = units[1][j];
-					units[0][j] = tempx;
-					units[1][j] = tempy;
-				}
-			}
-		}
+//		double tempx;
+//		double tempy;
+//		for(int i=0;i<99;i++){
+//			for(int j=i+1; j<100;j++){
+//				if (units[0][i]>units[0][j]){
+//					tempx = units[0][i];
+//					tempy = units[1][i];
+//					units[0][i] = units[0][j];
+//					units[1][i] = units[1][j];
+//					units[0][j] = tempx;
+//					units[1][j] = tempy;
+//				}
+//			}
+//		}
 		
 		//Training
-		int generations = 1;
-		double learnFactor = 0.01;
-		
-		for (int x = 0; x < generations; x++) {
-		
-			for (int i = 0; i < data[0].length; i++) {
-				double datax = data[0][i];
-				double datay = data[1][i];
-				
-				//finde Gewinner-Unit
-				int winnerIndex = 0;
-				double minDistance =  Double.POSITIVE_INFINITY;
-				for (int j = 0; j < units[0].length; j++) {
-					double currentDistance = getDistance(datax, datay, units[0][j],units[1][j]);
-					if (currentDistance< minDistance) {
-						winnerIndex = j;
-						minDistance = currentDistance;
-					}	
-					//wenn Unit überhalb Punkt liegt muss Unit runtergesetzt werden
-					if(datay > units[1][winnerIndex]){
-						minDistance = minDistance*-1;
-					}
-					System.out.println("minimale DIstanz: "+minDistance);
-					System.out.println("Datenpunkt "+i+" hat Gewinner-Unit mit Index "+winnerIndex);
-					units[1][j] = units[1][j] +minDistance*learnFactor;
-				}
-
-			}
-		}
-		
-		
-		
+					
 		
 		
 		//Visualisierung
